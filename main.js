c201bf23182d4a14d2363bb4c6fbb599faa3b831
Camila Villamayor <camilalvillamayor@gmail.com>
2024-07-02 20:17:58 -0300

    Se incorpora el resumen de pedido en la consola

--- main.js
+++ main.js
@@ -111,21 +111,14 @@
 
 
 //ENTEGA 3
-//let contador = 0;
-//document.getElementById()
-//const botonSumar = document.querySelector('#botonSumar');
+let contador = 0;
+let precioPizza=10000;
+let precioEmpanada=1000;
+let precioHamburguesa=7000;
+let precioHelado=1000;
 
-//const botonRestar = document.querySelector('#botonRestar');
 
-//botonSumar.addEventListener('click', () => {
-//contador++;
-//console.log(contador);
-//});
-
-//botonRestar.addEventListener('click', () => {
-//contador--;
-//console.log(contador);
-//});
+let precioTotal =0;
 
 //PIZZA
 
@@ -133,17 +126,21 @@ btn1A.addEventListener("click", () => {
     Swal.fire({
     icon: "success",
     title: "Éxito",
-    text: "Se agregó correctamente tu compra al carrito.El valor es de $10.000. Desea seguir comprando? "
+    text: "Se agregó correctamente tu compra .El valor es de $10.000. Desea seguir comprando? "
     });
-});
 
+    contador=contador + precioPizza;
+    console.log("Se agregó correctamente tu compra, el valor es de $10.000. Total a pagar es de: $ " + contador);
+});
 
 btn1B.addEventListener("click", () => {
     Swal.fire({
     icon: "error",
     title: "Eliminaste tu pedido",
-    text: "Eliminaste tu compra (PIZZA) del carrito."
+    text: "Eliminaste tu compra (PIZZA)."
     });
+    contador=contador - precioPizza;
+    console.log("Eliminaste tu compra (PIZZA). Total a pagar es de: $ " + contador);
 });
 
 //EMPANADAS
@@ -154,16 +151,21 @@ btn2A.addEventListener("click", () => {
     title: "Éxito",
     text: "Se agregó correctamente tu compra al carrito.El valor es de $1.000. Desea seguir comprando? "
     });
+
+    contador=contador + precioEmpanada;
+    console.log("Se agregó correctamente tu compra, el valor es de $1.000. Total a pagar es de: $ " + contador);
 });
 
 btn2B.addEventListener("click", () => {
     Swal.fire({
     icon: "error",
     title: "Eliminaste tu pedido",
-    text: "Eliminaste tu compra (EMPANADA/S) del carrito."
+    text: "Eliminaste tu compra (EMPANADA/S)."
     });
-});
 
+    contador=contador - precioEmpanada;
+    console.log("Eliminaste tu compra (EMPANADA/S).Total a pagar: $ " + contador);
+});
 
 //HAMBURGUESAS
 
@@ -173,17 +175,21 @@ btn3A.addEventListener("click", () => {
     title: "Éxito",
     text: "Se agregó correctamente tu compra al carrito.El valor es de $7.000. Desea seguir comprando? "
     });
+
+    contador=contador + precioHamburguesa;
+    console.log("Se agregó correctamente tu compra, el valor es de $7.000. Total a pagar es de: $ " + contador);
 });
 
 btn3B.addEventListener("click", () => {
     Swal.fire({
     icon: "error",
     title: "Eliminaste tu pedido",
-    text: "Eliminaste tu compra (HAMBURGUESA)del carrito."
+    text: "Eliminaste tu compra (HAMBURGUESA)."
     });
+
+    contador=contador - precioHamburguesa;
+    console.log("Eliminaste tu compra (HAMBURGUESA).Total a pagar: $ " + contador);
 });
-
-
 
 
 //CONO DE HELADO
@@ -194,102 +200,21 @@ btn4A.addEventListener("click", () => {
     title: "Éxito",
     text: "Se agregó correctamente tu compra al carrito.El valor es de $1.000. Desea seguir comprando? "
     });
+
+    contador=contador + precioHelado;
+    console.log("Se agregó correctamente tu compra, el valor es de $1.000. Total a pagar es de: $ " + contador);
 });
 
 btn4B.addEventListener("click", () => {
     Swal.fire({
     icon: "error",
     title: "Eliminaste tu pedido",
-    text: "Eliminaste tu compra del carrito."
-    });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-btn2.addEventListener("click", () => {
-    Swal.fire({
-    icon: "success",
-    title: "Éxito",
-    text: "Se agregó correctamente tu compra al carrito.El valor es de $1.000. Desea seguir comprando? "
+    text: "Eliminaste tu compra (CONO DE HELADO)."
     });
 
-    //Swal.fire({
-    //icon: "error",
-    //title: "Upps",
-    //text: "Hubo un error."
-    //});
+    contador=contador - precioHelado;
+    console.log("Eliminaste tu compra (CONO DE HELADO). Total a pagar: $ " + contador);
 });
 
-
-btn3.addEventListener("click", () => {
-    Swal.fire({
-    icon: "success",
-    title: "Éxito",
-    text: "Se agregó correctamente tu compra al carrito.El valor es de $7.000. Desea seguir comprando? "
-    });
-
-    //Swal.fire({
-    //icon: "error",
-    //title: "Upps",
-    //text: "Hubo un error."
-    //});
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Tipos de íconos: success, warning, error, info, question
-btn4.addEventListener("click", () => {
-    Swal.fire({
-    icon: "success",
-    title: "Éxito",
-    text: "Se agregó correctamente tu compra al carrito.El valor es de $1000. Desea seguir comprando? "
-    });
-
-    //Swal.fire({
-    //icon: "error",
-    //title: "Upps",
-    //text: "Hubo un error."
-    //});
-});
 
 
